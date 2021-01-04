b47ff46292152a9e27603183960059e0140ad394
Brady <thevillemc@gmail.com>
2021-01-04 20:03:35 +0000

    simple assertion function

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+const assertEqual = function(actual, expected) {
+  actual === expected ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+// Test Code
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1,1);
+assertEqual(1,'1');
+assertEqual('test', 'test');
+assertEqual(5, 6);
