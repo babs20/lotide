ef5f2989aae521380bf7a8d13fbeb95a57f61162
Brady <thevillemc@gmail.com>
2021-01-06 18:12:53 +0000

    added countLetters function

---
+++ countLetters.js
@@ -0,0 +1,32 @@
+const assertEqual = function (actual, expected) {
+  actual === expected ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const countLeters = sentence => {
+  if (typeof sentence !== 'string') {
+    return 'Please enter a string!';
+  }
+  
+  let letterTally = {};
+  let noSpaces = sentence.split(' ');
+  let stringInit = noSpaces.join('').toLowerCase();
+
+  for (const letter of stringInit) {
+    if (letterTally[letter]) {
+      letterTally[letter] += 1;
+    } else {
+      letterTally[letter] = 1;
+    }
+  }
+  return letterTally;
+};
+
+
+let result = countLeters('lighthouse in the house');
+assertEqual((result.h), 4);
+
+result = countLeters('LHL');
+assertEqual((result.h), 1);
+
+result = countLeters(5);
+assertEqual((result), 'Please enter a string!');
