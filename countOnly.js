4a90df9e02713ef2f253f478d54af2f620b0b4df
Brady <thevillemc@gmail.com>
2021-01-06 14:11:31 +0000

    added countOnly function

--- countOnly.js
+++ countOnly.js
@@ -1,10 +1,10 @@
-const assertEqual = function (actual, expected) {
+const assertEqual = function(actual, expected) {
   actual === expected ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
 };
 
 // allItems: an array of strings that we need to look through
 // itemsToCount: an object specifying what to count
-const countOnly = function (allItems, itemsToCount) {
+const countOnly = function(allItems, itemsToCount) {
   let results = {};
 
   for (const item of allItems) {
