0c53ddee5505c07e18303092e1a478d8983b7ee0
Brady <thevillemc@gmail.com>
2021-01-07 17:54:29 +0000

    added takeUntil function

---
+++ takeUntil.js
@@ -0,0 +1,38 @@
+const eqArrays = (arrayOne, arrayTwo) => {
+  if (JSON.stringify(arrayOne) === JSON.stringify(arrayTwo)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const assertArraysEqual = (actual, expected) => {
+  eqArrays(actual, expected) ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const takeUntil = function(array, callback) {
+  let results = [];
+  for (const item of array) {
+    if (!callback(item)) {
+      results.push(item);
+    } else {
+      return results;
+    }
+  }
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
+
+console.log('---');
+
+const data3 = [false, false, true, false];
+const results3 = takeUntil(data3, x => x === true);
+assertArraysEqual(results3, [false, false]);
