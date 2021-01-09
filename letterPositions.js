5f550bb6880044e693626fbe2e6f4a88e5193e3e
Brady <thevillemc@gmail.com>
2021-01-09 17:32:22 +0000

    added a conditional to check whether the character is a space and ignore it

--- letterPositions.js
+++ letterPositions.js
@@ -1,4 +1,4 @@
-const eqArrays = function(arrayOne, arrayTwo) {
+const eqArrays = function (arrayOne, arrayTwo) {
   if (JSON.stringify(arrayOne) === JSON.stringify(arrayTwo)) {
     return true;
   } else {
@@ -6,29 +6,31 @@ const eqArrays = function(arrayOne, arrayTwo) {
   }
 };
 
-const assertArraysEqual = function(actual, expected) {
+const assertArraysEqual = function (actual, expected) {
   eqArrays(actual, expected) ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
 };
 
-const letterPositions = function(sentence) {
+const letterPositions = function (sentence) {
   const results = {};
   for (const letter of sentence) {
-    if (results[letter]) {
-      let resultsArrary = results[letter];
-      let indexOfLastIndex = resultsArrary[resultsArrary.length - 1] + 1;
-      results[letter].push(sentence.indexOf(letter, indexOfLastIndex));
-    } else {
-      results[letter] = [sentence.indexOf(letter)];
+    if (letter !== ' ') {
+      if (results[letter]) {
+        let resultsArrary = results[letter];
+        let indexOfLastIndex = resultsArrary[resultsArrary.length - 1] + 1;
+        results[letter].push(sentence.indexOf(letter, indexOfLastIndex));
+      } else {
+        results[letter] = [sentence.indexOf(letter)];
+      }
     }
   }
   return results;
 };
 
 
-let results = letterPositions('helllo');
+let results = letterPositions('hell lo');
 assertArraysEqual(results.h, [0]);
 assertArraysEqual(results.e, [1]);
-assertArraysEqual(results.l, [2, 3, 4]);
-assertArraysEqual(results.o, [5]);
+assertArraysEqual(results.l, [2, 3, 5]);
+assertArraysEqual(results.o, [6]);
 
-assertArraysEqual(letterPositions("hello").e, [1]);
+console.log(letterPositions('hell lo'));
