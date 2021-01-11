29e3ecfa5d812e07fe26ff6d7781d321d9845a1d
Brady <thevillemc@gmail.com>
2021-01-11 17:33:45 +0000

    added mocha and chai functionality to tailTest.js

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,22 +1,51 @@
+const { expect } = require('chai');
 const assertEqual = require('../assertEqual');
 const tail = require('../tail');
 
-let result = tail(["Hello", "How", "Are", "You"]);
-assertEqual(result.length, 3);
-assertEqual(result[0], "How");
-assertEqual(result[1], "Are");
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+describe('#tail', function () {
+  context('testing array ["Hello", "How", "Are", "You"]', function () {
+    it('should return array length as 3', function () {
+      let result = tail(["Hello", "How", "Are", "You"]);
+      expect(result.length).to.deep.equal(3);
+    });
+    it(`should return first element of the array as 'How'`, function () {
+      let result = tail(["Hello", "How", "Are", "You"]);
+      expect(result[0]).to.deep.equal('How');
+    });
+    it(`should return second element of the array as 'Are'`, function () {
+      let result = tail(["Hello", "How", "Are", "You"]);
+      expect(result[1]).to.deep.equal('Are');
+    });
+  });
 
-result = tail([]);
-assertEqual(result.length, 0);
-assertEqual(result[0], undefined);
-assertEqual(result[1], undefined);
+  context('testing array []', function () {
+    it('should return array length as 0', function () {
+      let result = tail([]);
+      expect(result.length).to.deep.equal(0);
+    });
+    it(`should return first element of the array as undefined`, function () {
+      let result = tail([]);
+      expect(result[0]).to.deep.equal(undefined);
+    });
+    it(`should return second element of the array as undefined`, function () {
+      let result = tail([]);
+      expect(result[1]).to.deep.equal(undefined);
+    });
+  });
 
-result = tail(['One']);
-assertEqual(result.length, 0);
-assertEqual(result[0], undefined);
-assertEqual(result[1], undefined);
+  context(`testing array ['One']`, function () {
+    it('should return array length as 0', function () {
+      let result = tail(['One']);
+      expect(result.length).to.deep.equal(0);
+    });
+    it(`should return first element of the array as undefined`, function () {
+      let result = tail(['One']);
+      expect(result[0]).to.deep.equal(undefined);
+    });
+    it(`should return second element of the array as undefined`, function () {
+      let result = tail(['One']);
+      expect(result[1]).to.deep.equal(undefined);
+    });
+  });
+});
